3719bffee956e2e7dfad924221951ed2a17f20a7
vytautas.arminas <vytautas.arminas@gmai.com>
2019-05-02 23:52:39 +0200

    Added Derby DB

--- src/main/java/lt/arminai/moneyTransfer/persistence/AccountRepositoryImpl.java
+++ src/main/java/lt/arminai/moneyTransfer/persistence/AccountRepositoryImpl.java
@@ -3,10 +3,16 @@ package lt.arminai.moneyTransfer.persistence;
 import lt.arminai.moneyTransfer.model.Account;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.Optional;
 
 @ApplicationScoped
 public class AccountRepositoryImpl implements AccountRepository {
+
+    @PersistenceContext(name = "jpa-unit")
+    private EntityManager em;
+
     @Override
     public Optional<Account> getById(String accountId) {
         return Optional.of(new Account("1", "2", 40));
