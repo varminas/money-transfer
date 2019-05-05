0bae7d448f36395e8015258689c228deeb381bef
vytautas.arminas <vytautas.arminas@gmai.com>
2019-05-06 00:01:06 +0200

    Fixed transactions data uniqueness

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -7,4 +7,4 @@ INSERT INTO Account VALUES (2, '1000002', 2000, 'USD', 1, parsedatetime('2019-01
 INSERT INTO Account VALUES (3, '1000003', 1000, 'EUR', 2, parsedatetime('2019-01-05 20:04:35.69', 'yyyy-MM-dd hh:mm:ss.SS'), parsedatetime('2019-01-06 02:03:04.69', 'yyyy-MM-dd hh:mm:ss.SS'));
 
 INSERT INTO Transaction VALUES (1, 1, 2, 30, parsedatetime('2019-01-03 20:04:35.69', 'yyyy-MM-dd hh:mm:ss.SS'));
-INSERT INTO Transaction VALUES (1, 1, 3, 30, parsedatetime('2019-01-04 20:04:35.69', 'yyyy-MM-dd hh:mm:ss.SS'));
+INSERT INTO Transaction VALUES (2, 1, 3, 15, parsedatetime('2019-01-04 20:04:35.69', 'yyyy-MM-dd hh:mm:ss.SS'));
