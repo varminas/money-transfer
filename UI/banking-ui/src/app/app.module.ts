164d8fbdfc9cdfa0c8862eec3663bb29e56c8d01
a1v <a1v@suva.ch>
2019-05-14 17:08:25 +0200

    Added bootstrap

--- UI/banking-ui/src/app/app.module.ts
+++ UI/banking-ui/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
     declarations: [
@@ -16,6 +18,7 @@ import { HttpClientModule } from '@angular/common/http';
     ],
     imports: [
         BrowserModule,
+        NgbModule,
         AppRoutingModule,
         HttpClientModule,
         FormsModule
