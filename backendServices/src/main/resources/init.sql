eca46d065c95041945fcec3baed14142d0ad173b
vytautas.arminas <vytautas.arminas@gmai.com>
2019-05-17 22:44:28 +0200

    Add ADMIN role to user ob123

--- backendServices/src/main/resources/init.sql
+++ backendServices/src/main/resources/init.sql
@@ -2,6 +2,7 @@ INSERT INTO User VALUES ('bed6109f-ef8a-47ec-8fa4-e57c71415a10', 'ob123', 'PBKDF
 INSERT INTO User VALUES ('bed6109f-ef8a-47ec-8fa4-e57c71415a11', 'js456', 'PBKDF2WithHmacSHA256:2048:Dvy9IbwwIeaTPTYQXb64hIgAQ0QTqBUFJbptym5cAbo=:8eduhnKs3K6g9o2GxMtqCiOlGBbVK4bhx/w9uNfaTZw=', 'John', 'Snow', '+37060012341', 'M', parsedatetime('2019-02-03 15:04:35.69', 'yyyy-MM-dd hh:mm:ss.SS'), parsedatetime('2019-03-15 04:03:04.69', 'yyyy-MM-dd hh:mm:ss.SS'));
 
 INSERT INTO user_roles VALUES('aafdb294-92f1-469c-80fd-270a1e959601', 'ob123', 'USER', parsedatetime('2019-03-15 04:03:04.69', 'yyyy-MM-dd hh:mm:ss.SS'), parsedatetime('2019-03-15 04:03:04.69', 'yyyy-MM-dd hh:mm:ss.SS'));
+INSERT INTO user_roles VALUES('aafdb294-92f1-469c-80fd-270a1e959603', 'ob123', 'ADMIN', parsedatetime('2019-03-16 04:03:04.69', 'yyyy-MM-dd hh:mm:ss.SS'), parsedatetime('2019-03-16 04:03:04.69', 'yyyy-MM-dd hh:mm:ss.SS'));
 INSERT INTO user_roles VALUES('aafdb294-92f1-469c-80fd-270a1e959602', 'js456', 'USER', parsedatetime('2019-03-15 04:03:04.69', 'yyyy-MM-dd hh:mm:ss.SS'), parsedatetime('2019-03-15 04:03:04.69', 'yyyy-MM-dd hh:mm:ss.SS'));
 
 INSERT INTO Account VALUES ('bed6109f-ef8a-47ec-8fa4-e57c71415a12', '1000001', 4000, 'EUR', 'bed6109f-ef8a-47ec-8fa4-e57c71415a10', parsedatetime('2019-01-03 20:04:35.69', 'yyyy-MM-dd hh:mm:ss.SS'), parsedatetime('2019-01-04 02:03:04.69', 'yyyy-MM-dd hh:mm:ss.SS'), 0);
